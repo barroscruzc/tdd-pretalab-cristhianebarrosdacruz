d3661ca97752825eb02b592f20e448fc11076ece
barroscruzc <barroscruzc@gmail.com>
2024-07-13 16:17:04 -0300

    oficina TDD

--- calculadora.test.js
+++ calculadora.test.js
@@ -2,20 +2,22 @@ const Calculadora = require('./calculadora')
 
 describe('Calculadora', () => {
   it('Deverá retornar a soma de dois números', () => {
-    //Seu código aqui
-    
+    const calculadora = new Calculadora();
+    expect(calculadora.soma(5, 10)).toEqual(15);
   })
 
   it('Deverá retornar a subtração de dois números', () => {
-    //Seu código aqui
-    
+    const calculadora = new Calculadora();
+    expect(calculadora.subtracao(20, 3)).toEqual(17);
   })
 
   it('Deverá retornar a multiplicação entre dois números', () => {
-    //Seu código aqui
+    const calculadora = new Calculadora();
+    expect(calculadora.multiplicacao(20, 2)).toEqual(40);
   })
 
   it('Deverá retornar a divisao entre dois números', () => {
-    //Seu código aqui
+    const calculadora = new Calculadora();
+    expect(calculadora.divisao(20, 2)).toEqual(10);
   })
 })
--- queue.test.js
+++ queue.test.js
@@ -2,24 +2,32 @@ const Queue = require('./queue')
 
 describe('Queue', () => {
   it('Deve criar uma fila', () => {
-    const queue = new Queue()
+    const queue = new Queue();
 
     expect(queue).toEqual({ elements: []})
   })
 
   it('Deve ser capaz de calcular o tamanho da fila', () => {
-    // Seu código aqui
+    const queue = new Queue();
+    expect(queue.size()).toEqual(0);
   })
 
   it('Deve ser capaz de adicionar um item na fila', () => {
-   // Seu código aqui
+    const queue = new Queue();
+    queue.add('Maçã');
+    expect(queue).toEqual({ elements: ['Maçã']});
   })
 
   it('Deve escolher o primeiro item da fila', () => {
-    // Seu código aqui
+    const queue = new Queue();
+    queue.elements = ['Maçã', 'Abacaxi', 'Melancia'];
+    expect(queue.peek()).toEqual('Maçã');
   })
 
   it('Deve remover o primeiro item da fila', () => {
-    // Seu código aqui
+    const queue = new Queue();
+    queue.elements = ['Maçã', 'Abacaxi', 'Melancia'];
+    queue.dequeue();
+    expect(queue).toEqual({ elements: [ 'Abacaxi', 'Melancia' ]});
   })
 })
