8be2a62c7bbdf56a622556f64eaf92ba2e1cc961
barroscruzc <barroscruzc@gmail.com>
2024-07-13 16:54:07 -0300

    oficina TDD

--- queue.test.js
+++ queue.test.js
@@ -9,25 +9,55 @@ describe('Queue', () => {
 
   it('Deve ser capaz de calcular o tamanho da fila', () => {
     const queue = new Queue();
+
     expect(queue.size()).toEqual(0);
+
+    queue.add('Limão');
+    queue.add('Uva');
+
+    expect(queue.size()).toEqual(2);
   })
 
   it('Deve ser capaz de adicionar um item na fila', () => {
     const queue = new Queue();
+
     queue.add('Maçã');
+
     expect(queue).toEqual({ elements: ['Maçã']});
+
+    queue.add('Abacaxi');
+    queue.add('Laranja');
+
+    expect(queue).toEqual({ elements: ['Maçã', 'Abacaxi', 'Laranja']});
   })
 
   it('Deve escolher o primeiro item da fila', () => {
     const queue = new Queue();
-    queue.elements = ['Maçã', 'Abacaxi', 'Melancia'];
+
+    queue.add('Maçã');
+    queue.add('Abacaxi');
+    queue.add('Melancia');
+
     expect(queue.peek()).toEqual('Maçã');
+
+    queue.dequeue();
+
+    expect(queue.peek()).toEqual('Abacaxi');
   })
 
   it('Deve remover o primeiro item da fila', () => {
     const queue = new Queue();
-    queue.elements = ['Maçã', 'Abacaxi', 'Melancia'];
+
+    queue.add('Maçã');
+    queue.add('Abacaxi');
+    queue.add('Melancia');
+
     queue.dequeue();
+
     expect(queue).toEqual({ elements: [ 'Abacaxi', 'Melancia' ]});
+    
+    queue.dequeue();
+    
+    expect(queue.peek()).toEqual('Melancia');
   })
 })
